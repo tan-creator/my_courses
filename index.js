4ff2368984f46991aa297dae46cb8aaa4b0750b5
tan <77719969+tan-creator@users.noreply.github.com>
2022-06-02 15:55:27 +0700

    handlebars added

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+import * as path from 'path';
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//http logger
+app.use(morgan('combined'));
+//Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './src/views'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!/Bye World!')
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 app.listen(port, () => {
